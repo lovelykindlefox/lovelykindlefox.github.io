20f83b323942be6b3a76a5c3e3eef0bf7bd177e7
sybilwish <sybil.wish@gmail.com>
2016-11-21 13:49:05 +0800

    update

---
+++ scripts/lovelyKindleFox.js
@@ -0,0 +1,24 @@
+// ********************************************************************
+//
+// Javascript for LovelyKindleFox Github Page
+// Author: LovelyKindleFox
+//
+// ********************************************************************
+
+// UI Framework Generators
+var wow = new WOW({
+    boxClass: 'wow',
+    animateClass: 'animated',
+    offset: 0,
+    mobile: true,
+    live: true
+});
+wow.init();
+
+
+// UI Event
+
+// UI Event Effect 
+
+
+
