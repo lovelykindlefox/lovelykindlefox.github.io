702e5714833544f549f9e113ff000d3ab36fb879
sybilwish <sybil.wish@gmail.com>
2016-11-23 15:26:22 +0800

    update

--- scripts/lovelyKindleFox.js
+++ scripts/lovelyKindleFox.js
@@ -17,6 +17,31 @@ wow.init();
 
 
 // UI Event
+function resumeClose() {
+	$("#resumeArea").removeClass('sectionLarge');
+	$("#resumeArea").addClass('sectionBig');
+
+	$("#job03").hide();
+	$("#job04").hide();
+	$("#job05").hide();
+
+	$("#btnResumeClose").hide();
+	$("#btnResumeOpen").show();
+}
+
+function resumeOpen() {
+	$("#resumeArea").addClass('sectionLarge');
+	$("#resumeArea").removeClass('sectionBig');
+
+	$("#job03").show();
+	$("#job04").show();
+	$("#job05").show();
+
+	$("#btnResumeClose").show();
+	$("#btnResumeOpen").hide();
+}
+
+
 
 // UI Event Effect 
 
