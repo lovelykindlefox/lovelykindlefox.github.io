caa8193ee8c397699f95828b45cc92ffbb0bca5a
sybilwish <sybil.wish@gmail.com>
2016-11-27 23:54:04 +0800

    update

--- scripts/lovelyKindleFox.js
+++ scripts/lovelyKindleFox.js
@@ -16,10 +16,11 @@ var wow = new WOW({
 wow.init();
 
 
+
 // UI Event
 function resumeClose() {
-	$("#resumeArea").removeClass('sectionLarge');
-	$("#resumeArea").addClass('sectionBig');
+	// $("#resumeArea").removeClass('sectionLarge');
+	// $("#resumeArea").addClass('sectionBig');
 
 	$("#job03").hide();
 	$("#job04").hide();
@@ -27,11 +28,17 @@ function resumeClose() {
 
 	$("#btnResumeClose").hide();
 	$("#btnResumeOpen").show();
+
+	$("#resumeArea").css("height", "900px");
+	$("#resumeArea").css("background-size", "100% 900px");
+
+	$("#employmentArea").css("height", "480px");
+	$("#employmentArea").css("background-size", "100% 480px");
 }
 
 function resumeOpen() {
-	$("#resumeArea").addClass('sectionLarge');
-	$("#resumeArea").removeClass('sectionBig');
+	// $("#resumeArea").addClass('sectionLarge');
+	// $("#resumeArea").removeClass('sectionBig');
 
 	$("#job03").show();
 	$("#job04").show();
@@ -39,6 +46,14 @@ function resumeOpen() {
 
 	$("#btnResumeClose").show();
 	$("#btnResumeOpen").hide();
+
+
+	$("#resumeArea").css("height", "1600px");
+	$("#resumeArea").css("background-size", "100% 1600px");
+
+	$("#employmentArea").css("height", "1200px");
+	$("#employmentArea").css("background-size", "100% 1200px");
+
 }
 
 
